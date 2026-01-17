1cc5a6a8ae7ef89f49c3fd6e6487e55047cab315
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2026-01-17 16:04:38 +0000

    refactor: clean up code formatting and improve CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,89 @@
-import { RequestLoggerInterceptor } from './request-logger/request-logger.interceptor';
-import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {RequestLoggerInterceptor} from './request-logger/request-logger.interceptor';
+import {ConsoleLogger, ValidationPipe} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import * as compression from 'compression';
 import helmet from 'helmet';
-import { join } from 'path';
-import { PrismaExceptionFilter } from 'src/database/filters/prisma-exception.filter';
-import { AppModule } from './app.module';
+import {join} from 'path';
+import {PrismaExceptionFilter} from 'src/database/filters/prisma-exception.filter';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const isProduction = process.env.NODE_ENV === 'production';
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: new ConsoleLogger({
-      timestamp: true,
-      logLevels: isProduction
-        ? ['error', 'warn', 'log']
-        : ['error', 'warn', 'debug', 'verbose', 'log'],
-      json: isProduction,
-      prefix: 'onmec',
-      colors: !isProduction,
-    }),
-  });
+	const isProduction = process.env.NODE_ENV === 'production';
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		logger: new ConsoleLogger({
+			timestamp: true,
+			logLevels: isProduction
+				? ['error', 'warn', 'log']
+				: ['error', 'warn', 'debug', 'verbose', 'log'],
+			json: isProduction,
+			prefix: 'onmec',
+			colors: !isProduction,
+		}),
+	});
 
-  // injecter globalement ValidationPipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
+	// injecter globalement ValidationPipe
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			transformOptions: {enableImplicitConversion: true},
+		}),
+	);
 
-  // Appliquer le filtre globalement à toute l'application
-  app.useGlobalFilters(new PrismaExceptionFilter());
+	// Appliquer le filtre globalement à toute l'application
+	app.useGlobalFilters(new PrismaExceptionFilter());
 
-  // Security middleware
-  app.use(helmet({
-    originAgentCluster:false,
-  }));
+	// Security middleware
+	app.use(helmet({
+		crossOriginResourcePolicy: {
+			policy: 'cross-origin',
+		},
+	}));
 
-  // Compression
-  app.use(compression());
+	// Compression
+	app.use(compression());
 
-  // CORS
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:8080',
-      'https://admin.mec-ci.org',
-      'https://api.mec-ci.org',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+	// CORS
+	app.enableCors({
+		origin: [
+			'http://localhost:3000',
+			'http://localhost:8080',
+			'https://mec-ci.org',
+			'https://*.mec-ci.org',
+		],
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+	});
 
-  // API prefix
-  app.setGlobalPrefix('api/v1');
+	// API prefix
+	app.setGlobalPrefix('api/v1');
 
-  // Configuration du dossier de téléchargement
-  const uploadsPath = join(__dirname, '..', '..', 'uploads');
+	// Configuration du dossier de téléchargement
+	const uploadsPath = join(__dirname, '..', '..', 'uploads');
 
-  app.useStaticAssets(uploadsPath, {
-    prefix: '/uploads',
-  });
+	app.useStaticAssets(uploadsPath, {
+		prefix: '/uploads',
+	});
 
-  app.useGlobalInterceptors(new RequestLoggerInterceptor());
+	app.useGlobalInterceptors(new RequestLoggerInterceptor());
 
-  // Liaison du Swagger
-  const config = new DocumentBuilder()
-    .setTitle('OnMec API')
-    .setDescription('The API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .addServer('https://api.mec-ci.org', 'Production server')
-    .addServer('http://localhost:8081', 'Local server')
-    .build();
+	// Liaison du Swagger
+	const config = new DocumentBuilder()
+		.setTitle('OnMec API')
+		.setDescription('The API description')
+		.setVersion('1.0')
+		.addBearerAuth()
+		.addServer('https://api.mec-ci.org', 'Production server')
+		.addServer('http://localhost:8081', 'Local server')
+		.build();
 
-  const documentFactory = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, documentFactory);
+	const documentFactory = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api/docs', app, documentFactory);
 
-  // Lancer le serveur
-  await app.listen(process.env.PORT ?? 8081);
+	// Lancer le serveur
+	await app.listen(process.env.PORT ?? 8081);
 }
 
 void bootstrap();
